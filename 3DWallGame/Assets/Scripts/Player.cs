47de7ca7e162e1ddef79269dceb14bbcb1e06388
KimTaeJun <zzaxowns@naver.com>
2021-03-28 18:25:27 +0900

    플레이어랑 벽 제작
    플레이어랑 벽 제작했음

---
+++ 3DWallGame/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float jumpPower;
+
+    // 처음 한번만 실행 되는 함수
+    void Start()
+    {
+    }
+
+    // 매 frame마다 실행 되는 함수
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, jumpPower, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        SceneManager.LoadScene("Main"); // 맨 처음 모습으로 돌아옴
+
+    }
+}
